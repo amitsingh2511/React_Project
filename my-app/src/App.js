ecaeedd5059fb2622eb3028329aa36f9675592cd
amit251198 <amit.singh@faarms.com>
2023-05-26 16:55:27 +0530

    basic setup

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Hello World</h2>
+        <h1>Amit React First Project</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          React Tutorial
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
